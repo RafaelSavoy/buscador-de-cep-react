fcd979746fd5641ba62ac5a694cb0eba16a6aa6b
Rafael Savoy <rafaelsavoy1@outlook.com>
2022-07-29 15:56:12 -0300

    Refatorando código para deixa-lo menor

--- src/components/CepInfo/CepInfo.jsx
+++ src/components/CepInfo/CepInfo.jsx
@@ -4,13 +4,13 @@ import { CepData, CepForm, CepInfoDiv } from './styles';
 
 const Cep = () => {
 	const { cep } = useContext(CepContext)
-	const cepDatas = ['Rua','Bairro','Cidade','Estado'];
+	const cepDatas = ['Rua', 'Bairro', 'Cidade', 'Estado'];
 	return (
 		<CepInfoDiv>
 			<CepForm>
 				{cepDatas.map(cepData => {
-				 	const lowerCaseCepData = cepData.toLowerCase()
-					<CepData type='text' name={lowerCaseCepData} placeholder={cepData} value={lowerCaseCepData} />
+					const lowerCaseCepData = cepData.toLowerCase()
+					return <CepData type='text' name={lowerCaseCepData} placeholder={cepData} value={cep[lowerCaseCepData]} />
 				})}
 			</CepForm>
 		</CepInfoDiv>
